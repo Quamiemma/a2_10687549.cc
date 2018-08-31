1f65fadc0b4165f6737d6befee5fe1c6b8d018ff
Quamiemma <42769719+Quamiemma@users.noreply.github.com>
2018-08-31 02:02:37 +0000

    gradeSystem.cpp

---
+++ gradeSystem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int mark;
+    //ask for the user input and stroe
+    //it in mark
+    cout << "Enter your score : " << endl;
+    cin mark;
+    
+    if (mark >= 80 && mark <= 100){
+         cout << "A" << endl;
+    }
+    else if (mark >= 70 && mark <= 79){
+        cout << "B" << endl;
+    }
+  else if (mark >= 55 && mark <= 69){
+        cout << "C" << endl;
+    }
+   else if (mark >= 40 && mark <= 54){
+        cout << "D" << endl;
+    }
+    else{
+        cout << "F" << endl;
+    }
+}
